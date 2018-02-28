83937408b84911b312bc67374561e9b9987b2e3d
Oles Marola <omarola@cogniance.com>
2018-02-28 18:30:12 +0200

    Item entity and getAllItem method was added v0.2

---
+++ src/AppBundle/Entity/Item.php
@@ -0,0 +1,186 @@
+<?php
+
+namespace AppBundle\Entity;
+
+class Item
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var int
+     */
+    private $price;
+
+    /**
+     * @var string
+     */
+    private $color;
+
+    /**
+     * @var integer
+     */
+    private $size;
+
+    /**
+     * @var integer
+     */
+    private $weight;
+
+    /**
+     * @var Category
+     */
+    protected $category;
+
+    /**
+     * @param Category $category
+     */
+    public function setCategory(Category $category) {
+        $this->category = $category;
+    }
+
+    /**
+     * @return Category
+     */
+    public function getCategory() {
+        return $this->category;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Item
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set price
+     *
+     * @param integer $price
+     *
+     * @return Item
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return int
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    /**
+     * Set color
+     *
+     * @param string $color
+     *
+     * @return Item
+     */
+    public function setColor($color)
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    /**
+     * Get color
+     *
+     * @return string
+     */
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    /**
+     * Set size
+     *
+     * @param string $size
+     *
+     * @return Item
+     */
+    public function setSize($size)
+    {
+        $this->size = $size;
+
+        return $this;
+    }
+
+    /**
+     * Get size
+     *
+     * @return string
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * Set weight
+     *
+     * @param string $weight
+     *
+     * @return Item
+     */
+    public function setWeight($weight)
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    /**
+     * Get weight
+     *
+     * @return string
+     */
+    public function getWeight()
+    {
+        return $this->weight;
+    }
+}
+
