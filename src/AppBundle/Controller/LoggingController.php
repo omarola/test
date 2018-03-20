11060953b89348bef343b525a4cbdeb70d6f06c9
Oles Marola <omarola@cogniance.com>
2018-03-20 23:21:42 +0200

    Annotation changes v0.8.1

--- src/AppBundle/Controller/LoggingController.php
+++ src/AppBundle/Controller/LoggingController.php
@@ -8,8 +8,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\View\View;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * Class LoggingController
+ * @package AppBundle\Controller
+ */
 class LoggingController extends Controller
 {
+    /**
+     * @return View
+     */
     public function getLogAction()
     {
         $content = $this->getDoctrine()->getRepository(ChangeLog::class)->findAll();
